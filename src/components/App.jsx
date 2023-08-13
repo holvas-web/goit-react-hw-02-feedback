e93e44c1bd748349026a6df12c2bc85baf9d176f
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-08-13 12:25:34 +0200

    function & lib

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@
 import { Component } from "react";
 import { Container } from './GlobalStyle';
 import { SectionWrap } from '../SectionWrap/SectionWrap';
-import { FeedbackOptions } from '../FeedbackOptions/FeedbackOptions';
+import { FeedbackOptions } from '../Feedback/FeedbackOptions';
 import { Statistics } from '../Statistics/Statistics';
 import { Notification } from '../Notification/Notification';
 
@@ -56,6 +56,7 @@ export class App extends Component {
             options={options}
           />
         </SectionWrap>
+        
         <SectionWrap title="Statistics">
           {totalFeedback !== 0 ? (
             <Statistics
